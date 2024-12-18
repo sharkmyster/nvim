e0d8bd0e2a5dddb802a58ca1567dafb505129645
DeliDanny <daniel.sharkey@deliveroo.co.uk>
2024-12-18 14:57:05 +0000

    Blame config

--- lua/danny/plugins/blame.lua
+++ lua/danny/plugins/blame.lua
@@ -1,1 +1,9 @@
-return { 'f-person/git-blame.nvim' }
+return {
+  'f-person/git-blame.nvim',
+  event = 'VeryLazy',
+  opts = {
+    enable = true,
+    message_template = "<<sha>> • <author> • <date> • <summary>",
+    date_format = "%r",
+  }
+}

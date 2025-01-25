70caa3124b1e1de9af7e64e07daf6a66bd7bc6d7
DeliDanny <daniel.sharkey@deliveroo.co.uk>
2025-01-25 18:15:13 +0000

    blame

--- lua/danny/plugins/blame.lua
+++ lua/danny/plugins/blame.lua
@@ -1,20 +1,12 @@
 return {
   {
     "f-person/git-blame.nvim",
-    -- load the plugin at startup
     event = "VeryLazy",
-    -- Because of the keys part, you will be lazy loading this plugin.
-    -- The plugin wil only load once one of the keys is used.
-    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
-    -- or lazy = false. One of both options will work.
     opts = {
-      -- your configuration comes here
-      -- for example
-      enabled = 1, -- if you want to enable the plugin
-      message_template = " <summary>  <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-      date_format = "%r", -- template for the date, check Date format section for more options
-      virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
+      enabled = 1,
+      message_template = " <summary> • <date> • <author>",
+      date_format = "%r",
+      virtual_text_column = 1,
     },
   }
-
 }

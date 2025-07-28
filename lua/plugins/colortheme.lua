fb902c71e1f03881045c31f8ef4420eaa3a4f81f
Daniel Sharkey <dsharkey@protonmail.com>
2025-07-28 08:52:15 +0100

    Config updates

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,20 +1,19 @@
 return {
-  "Shatur/neovim-ayu",
-  lazy = false,    -- Load the plugin immediately
-  priority = 1000, -- Ensure the theme is loaded first
-  config = function()
-    require('ayu').setup({
-      mirage = true, -- Use Ayu Mirage variant
-      overrides = {
-
-      },                       -- Additional color overrides
-    })
-    vim.cmd("colorscheme ayu") -- Set the colorscheme
-    vim.api.nvim_set_hl(0, "@markup.link.label", { underline = false })
-    vim.api.nvim_set_hl(0, "@string.special.url", { underline = false })
-    vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { underline = true, sp = "#ff9e64" })
-    vim.api.nvim_set_hl(0, "LineNr", { fg = "#4db8d5" })
-    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#4db8d5" })
-    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#4db8d5" })
-  end,
+	"Shatur/neovim-ayu",
+	lazy = false, -- Load the plugin immediately
+	priority = 1000, -- Ensure the theme is loaded first
+	config = function()
+		require("ayu").setup({
+			mirage = true, -- Use Ayu Mirage variant
+			overrides = {}, -- Additional color overrides
+		})
+		vim.cmd("colorscheme ayu") -- Set the colorscheme
+		vim.api.nvim_set_hl(0, "@markup.link.label", { underline = false })
+		vim.api.nvim_set_hl(0, "@string.special.url", { underline = false })
+		vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { underline = true, sp = "#ff9e64" })
+		vim.api.nvim_set_hl(0, "LineNr", { fg = "#4db8d5" })
+		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#4db8d5" })
+		vim.api.nvim_set_hl(0, "SpecialKey", { fg = "#e6b450" })
+		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#4db8d5" })
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,15 @@ return {
 			ensure_installed = { "lua", "python", "javascript", "go", "typescript", "tsx", "bash", "css", "html" },
 			highlight = { enable = true },
 			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>", -- Start selection
+					node_incremental = "<C-space>", -- Expand to parent
+					scope_incremental = "<C-s>", -- Expand to scope
+					node_decremental = "<BS>", -- Shrink
+				},
+			},
 		})
 	end,
 }
